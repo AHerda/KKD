9a1d7260efcb818b9d8ab67c48c7a857b93e8109
AHerda <adrianherda@gmail.com>
2023-10-12 00:17:55 +0200

    Started read_dir

--- lista1/src/libs/helpers.rs
+++ lista1/src/libs/helpers.rs
@@ -1,35 +1,76 @@
+#[derive(Debug, PartialEq)]
+enum PathType {
+    File,
+    Dir,
+    None,
+}
+
 #[derive(Debug)]
 pub struct Dane {
-    pub file_path: String,
+    pub path_type: PathType,
+    pub path: String,
     pub table: bool,
 }
 
 pub fn read_args() -> Dane {
-    let mut args = std::env::args();
-    let mut file_path: Option<String> = None;
-    let mut table = false;
+    let mut args: std::env::Args = std::env::args();
+
+    let mut path_type: PathType = PathType::None;
+    let mut path: Option<String> = None;
+    let mut table: bool = false;
+
     while let Some(s) = args.next() {
         if s == "--table" {
             table = true;
         } else if s == "--src" {
-            file_path = args.next();
+            path = args.next();
+            if path_type == PathType::None {
+                path_type = PathType::File;
+            } else {
+                eprintln!("Error: You can onlu specify ONE path");
+                std::process::exit(1);
+            }
+        } else if s == "--src-dir" {
+            path = args.next();
+            if path_type == PathType::None {
+                path_type = PathType::Dir;
+            } else {
+                eprintln!("Error: You can onlu specify ONE path");
+                std::process::exit(1);
+            }
         }
     }
 
-    match file_path {
-        Some(s) => Dane {
-            file_path: s,
+    let path: String = path.unwrap_or_else(|| {
+        eprintln!("Error: the path must be specified after [--src] or [--src-dir] flag");
+        std::process::exit(1);
+    });
+
+    match path_type {
+        PathType::None => {
+            eprintln!("Error: the path must be specified with [--src] or [--src-dir] flag");
+            std::process::exit(1);
+        },
+        _ => Dane {
+            path_type,
+            path,
             table,
         },
-        None => {
-            eprintln!("Error: the file must be specified with [--src] flag");
-            std::process::exit(1);
-        }
     }
 }
 
 pub fn read_file(file_path: &str) -> Vec<u8> {
     match std::fs::read(file_path) {
+        Ok(v) => v,
+        Err(err) => {
+            eprintln!("{err}");
+            std::process::exit(1);
+        }
+    }
+}
+
+pub fn read_dir(dir_path: &str) -> Vec<u8> {
+    match std::fs::read_dir(dir_path) {
         Ok(v) => v,
         Err(err) => {
             eprintln!("{err}");
